7f8b1e00dce3102199880aa8cb3fd2bc4ad4b668
TakeNewcare <163362484+TakeNewcare@users.noreply.github.com>
2024-06-17 10:42:43 +0900

    Add files via upload

---
+++ 내가 만든 것/Cell.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mine
+{
+    // 각 칸을 하나하나의 버튼으로 만들기 위해 cell이라는 객체 생성
+    internal class Cell : Button
+    {
+        public const int cell_size = 25;
+
+        public string hidden_text;
+        public int x;
+        public int y;
+
+
+        public Cell(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+            Make_Cell(x, y);
+        }
+
+
+        private void Make_Cell(int x, int y)
+        {
+            this.Location = new System.Drawing.Point(x * cell_size, y * cell_size + Form1.menu_height + display.HEIGHT + 10);
+            this.Name = "Cell";
+            this.Size = new System.Drawing.Size(cell_size + 3, cell_size + 3);
+            this.TabIndex = 0;
+            this.Text = "";
+            this.UseVisualStyleBackColor = true;
+
+        }
+
+
+
+    }
+}
---
+++ 내가 만든 것/Level_Button.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mine
+{
+
+    // 폼2에 사용되는 버튼 객체 정의
+    internal class Level_Button : Button
+    {
+        public int level;
+        public int mines;
+
+        public const int w = 180;
+        public const int h = 50;
+
+        public Level_Button(string text, int x, int y, int level)
+        {
+            this.level = level;
+
+            this.Location = new System.Drawing.Point(x, y);
+            this.Name = "Button";
+            this.Size = new System.Drawing.Size(180, 50);
+            this.TabIndex = 1;
+            this.Text = text;
+            this.UseVisualStyleBackColor = true;
+
+            // 생성 시 전달 받은 레벨로 마인 갯수 설정
+            switch (level)
+            {
+                case 10:
+                    this.mines = 10;
+                    break;
+                case 15:
+                    this.mines = 45;
+                    break;
+                case 20:
+                    this.mines = 60;
+                    break;
+            }
+        }
+
+
+
+    }
+}
